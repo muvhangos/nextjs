3221c1fa393b945f7ec3c3001586a070283c86a4
Samuel Muvhango <93419130+muvhangos@users.noreply.github.com>
2026-05-26 11:55:42 +0200

    Add files via upload

---
+++ Chapter03/app/page.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  )
+};
+
+export default HomePage;
